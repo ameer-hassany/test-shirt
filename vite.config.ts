4db0c252b973faf25351adabeacd0c7f1d13833c
ameer-hassany <ameer_hassany@outlook.com>
2025-10-07 14:51:47 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/test-shirt/', // 👈 السطر السحري لحل مشكلة الشاشة البيضا
+    base: '/test-shirt/', // 👈 السطر اللي بيحل مشكلة الشاشة البيضا
     server: {
       port: 3000,
       host: '0.0.0.0',
